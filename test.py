23b2af007cf8624b881760a893624996c39b485f
HabibaAhmed12 <22-101084@students.eui.edu.eg>
2025-05-13 21:59:59 +0300

    Push file

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-#dubdbd
+from manim import *
+from manim.utils.space_ops import rotate_vector
 
-#dhbfeuifigrt
+class KochFractal(Scene):
+    def koch(self, p1, p2, level):
+        if level == 0:
+            return [Line(p1, p2)]
+        else:
+            a = p1
+            b = interpolate(p1, p2, 1 / 3)
+            d = interpolate(p1, p2, 2 / 3)
+            direction = d - b
+            # Rotate by 60 degrees counterclockwise
+            c = b + rotate_vector(direction, PI / 3)
+            return (self.koch(a, b, level - 1) +
+                    self.koch(b, c, level - 1) +
+                    self.koch(c, d, level - 1) +
+                    self.koch(d, p2, level - 1))
+
+    def construct(self):
+        p1 = LEFT * 4
+        p2 = RIGHT * 4
+        lines = self.koch(p1, p2, 3)
+        self.play(*[Create(line) for line in lines])
+        self.wait()
